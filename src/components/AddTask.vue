3a6af6819f6624acf994fd00157b0aeba25adcb5
FactoriaF5 <119811552+diegoFactoriaf5@users.noreply.github.com>
2023-08-28 11:20:47 +0200

    Fix and Update AddTask.vue

--- src/components/AddTask.vue
+++ src/components/AddTask.vue
@@ -1,34 +1,51 @@
 <script setup>
 import TaskDataService from "../services/TaskDataService";
-import { ref, onBeforeMount } from 'vue';
+import { reactive } from 'vue';
 
-const newTask = ref();
 
-function createNewTask() {
-    TaskDataService.create(newTask.value)
-    .then(response =>{
+const task = reactive({
+    id: null,
+    title: "",
+    description: "",
+})
 
-        console.log(response.data);
-        newTask.value.tittle = response.data.tittle
-        newTask.value.description = response.data.description
-    })
+function saveTask() {
+    var data = {
+        title: task.title,
+        description: task.description
+    };
 
-   
+    TaskDataService.create(data)
+        .then(response => {
+            task.id = response.data.id;
+            console.log(response.data);
+        })
+        .catch(e => {
+            console.log(e);
+        });
 }
 
 </script>
 
 <template>
-    <form @submit.prevent="createNewTask">
+    <div>
         <div>
-            <label>Title</label>
-            <input type="text" id="title" v-model="newTask.tittle" />
+            <label>Title </label>
+            <input type="text" id="title" required v-model="task.title" name="title" />
         </div>
+
         <div>
-            <label>Description</label>
-            <input type="text" id="description" v-model="newTask.description" />
+            <label>Description </label>
+            <input id="description" required v-model="task.description" name="description" />
         </div>
-        <button type="submit">Submit</button>
-    </form>
-   
+
+        <button @click="saveTask">Submit</button>
+    </div>
 </template>
+
+<style>
+.submit-form {
+    max-width: 300px;
+    margin: auto;
+}
+</style>
